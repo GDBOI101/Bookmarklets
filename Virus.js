446a4dfa982276b4d0b94b4f12d70afc4b622b41
GD_Studios <61432333+GD-Studios@users.noreply.github.com>
2021-05-19 11:00:44 -0700

    Add files via upload

---
+++ Virus.js
@@ -0,0 +1,67 @@
+javascript:var BB = 10; var i = 10; var CC = 0; alert("Turn up volume to max and press OK to disable go guardian"); alert("Go Guardian Disabled"); while (i > 0) { var win = window.open("https://codehs.com/uploads/b2679f56b6f608ba1ba3a27c39ae7590");i = i -1; var win2; var timer = setInterval(function() {   
+    if(win.closed) {  
+        while(BB > 0) {
+            alert("We're no strangers to computer virus 
+You know the rules and so do I 
+A full commitment's what I'm thinking of 
+You wouldn't get this from any other guy 
+I just wanna tell you how I'm feeling 
+Gotta make you understand 
+Always gonna give you virus 
+Always gonna let you down 
+Always gonna run around and desert you 
+Always gonna make you cry 
+Always gonna say goodbye 
+Always gonna tell a lie and hurt you 
+We've known each other for so long 
+Your heart's been aching but you're too shy to say it 
+Inside we both know what's been going on 
+We know the game and we're gonna play it 
+And if you ask me how I'm feeling 
+Don't tell me you're too blind to see 
+Always gonna give you Virus 
+Always gonna let you down 
+Always gonna run around and desert you 
+Always gonna make you cry 
+Always gonna say goodbye 
+Always gonna tell a lie and hurt you 
+Always gonna give you Always 
+Always gonna let you down 
+Always gonna run around and desert you 
+Always gonna make you cry 
+Always gonna say goodbye 
+Always gonna tell a lie and hurt you 
+Always gonna give, never gonna give 
+(Give you Virus) 
+(Ooh) Always gonna give, Always gonna give 
+(Give you Virus) 
+We've known each other for so long 
+Your heart's been aching but you're too shy to say it 
+Inside we both know what's been going on 
+We know the game and we're gonna play it 
+I just wanna tell you how I'm feeling 
+Gotta make you understand 
+Always gonna give you Virus 
+Always gonna let you down 
+Always gonna run around and desert you 
+Always gonna make you cry 
+Always gonna say goodbye 
+Always gonna tell a lie and hurt you 
+Always gonna give you Virus 
+Always gonna let you down 
+Always gonna run around and desert you 
+Always gonna make you cry 
+Always gonna say goodbye 
+Always gonna tell a lie and hurt you 
+Always gonna give you Virus 
+Always gonna let you down 
+Always gonna run around and desert you 
+Always gonna make you cry ");
+            BB = BB -1
+        }
+        if(BB < 2) {
+            alert("Crashing...");
+            var i = 0; while (i < 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999) { window.location.reload(true); i = i - 1 };
+        }
+    }  
+}, 1000);};
